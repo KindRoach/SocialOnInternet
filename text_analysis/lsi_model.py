042669bcb07ae1543ec1f36c2c726f10275defc8
KindRoach <kindroach@hotmail.com>
2017-04-13 21:35:40 +0800

    使用迭代器减少了LSI话题模型的内存占用

--- text_analysis/lsi_model.py
+++ text_analysis/lsi_model.py
@@ -1,6 +1,8 @@
 import os
 import re
 import csv
+from itertools import tee
+
 import jieba
 import zhon.hanzi as chinese
 from gensim import corpora
@@ -23,7 +25,9 @@ def jieba_tokenizer(doc):
 
 stop_words = get_stop_words()
 re_chinese = re.compile('[%s]' % chinese.characters)
+re_gov = re.compile("公知")
 
+# 保存dictionary和corpus，以供随后使用
 dict_path = r"../data/weibo.dict"
 mm_path = r"../data/weibo.mm"
 if os.path.exists(dict_path):
@@ -31,13 +35,14 @@ if os.path.exists(dict_path):
     corpus = corpora.MmCorpus(mm_path)
 else:
     reader = csv.reader(open(r"../data/weibo.csv", encoding="UTF8"), delimiter=",")
-    texts = [jieba_tokenizer(line[0] + line[4]) for line in reader]
+    texts = (jieba_tokenizer(line[0] + line[4]) for line in reader if re_gov.search(line[0]) or re_gov.search(line[4]))
+    texts, texts_backup = tee(texts)
     dictionary = corpora.Dictionary(texts)
     dictionary.save(r"../data/weibo.dict")
-    corpus = [dictionary.doc2bow(text) for text in texts]
+    corpus = [dictionary.doc2bow(text) for text in texts_backup]
     corpora.MmCorpus.serialize(mm_path, corpus)
 
 lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=10)
 topics = lsi.show_topics()
 for topic in topics:
-    print(topic)
+    print(topic[1])
