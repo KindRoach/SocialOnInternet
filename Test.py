a877f05593b2cb30f3f1346e54ee3d657cefaad2
KindRoach <kindroach@hotmail.com>
2017-04-13 21:35:33 +0800

    使用迭代器减少了LSI话题模型的内存占用

--- Test.py
+++ Test.py
@@ -1,17 +1,13 @@
-from gensim import corpora
-from gensim import models
+import re
 
-texts = [['human', 'interface', 'computer'],
-         ['survey', 'user', 'computer', 'system', 'response', 'time'],
-         ['eps', 'user', 'interface', 'system'],
-         ['system', 'human', 'system', 'eps'],
-         ['user', 'response', 'time'],
-         ['trees'],
-         ['graph', 'trees'],
-         ['graph', 'minors', 'trees'],
-         ['graph', 'minors', 'survey']]
-dictionary = corpora.Dictionary(texts)
-corpus = [dictionary.doc2bow(text) for text in texts]
-lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-text1 = lsi[corpus[0]]
-print(text1)
+s = "中国政府警告"
+ss = "中国政府官员"
+sss = "没有"
+
+re_gov = re.compile("政府|官员")
+if re_gov.search(s):
+    print(s)
+if re_gov.search(ss):
+    print(ss)
+if re_gov.search(sss):
+    print(sss)
